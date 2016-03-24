8fb6a3bbaa4139bcd70d9eaa3093cc2ac9eba8c9
李营 <liying@liyingdeMacBook-Pro.local>
2016-03-24 08:59:32 +0800

    Initial commit

---
+++ app/src/main/java/com/example/liying/myapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.liying.myapplication;
+
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        sdf();
+    }
+
+    private void sdf() {
+        String sss = "  sdsdf ";
+    }
+
+
+}
